b301487756a0acd90bd7aa7f4de567f7fb862127
Yama022 <r-jacquemin@hotmail.fr>
2022-02-16 17:49:01 +0100

    OK/ SCSS Crew component nav

--- src/components/Crew/choice.js
+++ src/components/Crew/choice.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { RiCheckboxBlankCircleFill, RiCheckboxBlankCircleLine } from 'react-icons/ri';
+import { useLocation } from "react-router-dom";
 
 import './styles.scss';
 
 export default function choice() {
+  //assigning location variable
+  const location = useLocation();
+
+  //destructuring pathname from location
+  const { pathname } = location;
+
+  //Javascript split method to get the name of the path in array
+  const splitLocation = pathname.split("/");
   return (
     <div className="crew__content__title">
             <nav className="crew__content__title__nav">
-              <NavLink to="/crew/douglas" className="crew__content__title__select" exact>
-                <h5><RiCheckboxBlankCircleFill/></h5>
-              </NavLink>
-              <NavLink to="/crew/mark" className="crew__content__title__select" exact>
+              <NavLink to="/crew/douglas" className={splitLocation[1] === "" ? "active" : ""} exact>
                 <h5><RiCheckboxBlankCircleLine/></h5>
               </NavLink>
-              <NavLink to="/crew/victor" className="crew__content__title__select" exact>
+              <NavLink to="/crew/mark" className={splitLocation[1] === "" ? "active" : ""} exact>
                 <h5><RiCheckboxBlankCircleLine/></h5>
               </NavLink>
-              <NavLink to="/crew/anousheh" className="crew__content__title__select" exact>
+              <NavLink to="/crew/victor" className={splitLocation[1] === "" ? "active" : ""} exact>
+                <h5><RiCheckboxBlankCircleLine/></h5>
+              </NavLink>
+              <NavLink to="/crew/anousheh" className={splitLocation[1] === "" ? "active" : ""} exact>
                 <h5><RiCheckboxBlankCircleLine/></h5>
               </NavLink>
             </nav>
